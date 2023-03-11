61845b9890d38c23d17d811c0ac61ebbfe73ab08
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-03-11 11:36:00 -0600

    remove default syntax, create new project page to contain all projects

---
+++ src/pages/projects.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title>Projects</title>
+        <meta
+          name="description"
+          content="page containing all projects created by jorge mendoza"
+        />
+      </Head>
+    </>
+  );
+}

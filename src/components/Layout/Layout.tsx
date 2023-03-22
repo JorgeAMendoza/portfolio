20bf91b8f120b9ddd9049f1413638ffef6565741
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-03-22 05:25:57 -0500

    remove navbar container from layout component, give navbar component its own container in the module css, style the basic structure and position the mobile menu

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import { ReactNode } from 'react';
 import NavBar from '../NavBar/NavBar';
 import { Open_Sans } from 'next/font/google';
-import styles from './Layout.module.css';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -17,7 +16,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.navContainer}>
+      <header>
         <NavBar />
       </header>
       <div>{children}</div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,38 +1,54 @@
 import Image from 'next/image';
+import utilStyles from '../../styles/utils.module.css';
+import styles from './navbar.module.css';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div>
-      <div>
-        <Image src="/logo.svg" alt="jorge mendoza logo" width={188} height={40} />
-      </div>
-
-      {/* mobile nav */}
-      <div>
-        <button
-          aria-label="open the navigation menu"
-          aria-controls="mobile-nav"
-        >
+    <div className={styles.navBar}>
+      <div className={styles.navBarContainer}>
+        <div>
           <Image
-            src="/icons/hamburger-menu.svg"
-            alt="menu icon"
-            width={30}
-            height={30}
+            src="/logo.svg"
+            alt="jorge mendoza logo"
+            width={148}
+            height={40}
           />
-        </button>
-        <nav id="mobile-nav">
-          <ul>
-            <li>About</li>
-            <li>Showcase</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
-
-      {/* desktop nav */}
-      <div>
-        <nav>
+        </div>
+        {/* mobile nav */}
+        <div className={styles.mobileNav}>
+          <button
+            className={styles.mobileNavButton}
+            aria-label="open the navigation menu"
+            aria-controls="mobile-nav"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <Image
+              src="/icons/hamburger-menu.svg"
+              alt="menu icon"
+              width={30}
+              height={30}
+            />
+          </button>
+          <nav
+            id="mobile-nav"
+            className={`${styles.mobileNavMenu} ${clsx(
+              menuOpen && styles.showMenu
+            )}`}
+            aria-hidden={menuOpen}
+          >
+            <ul>
+              <li>About</li>
+              <li>Showcase</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+        {/* desktop nav */}
+        <nav className={styles.nav}>
           <ul>
             <li>About</li>
             <li>Showcase</li>

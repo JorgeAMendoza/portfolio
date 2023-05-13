7fc53bb9186289a3730e1a1d929f2885c09cb909
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-05-13 13:07:24 -0500

    use get static props and paths to generate pages for showcase projects, only basic markdown is rendered as of now

---
+++ src/pages/project/[slug].tsx
@@ -0,0 +1,61 @@
+import path from 'path';
+import fs from 'fs';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import matter from 'gray-matter';
+import { InferGetStaticPropsType } from 'next';
+
+interface ShowcasePageProps {
+  source: MDXRemoteSerializeResult<
+    Record<string, unknown>,
+    Record<string, unknown>
+  >;
+  frontMatter: {
+    title: string;
+    tools: string[];
+  };
+}
+
+const ShowcasePage = ({
+  source,
+  frontMatter,
+}: ShowcasePageProps): InferGetStaticPropsType<typeof getStaticProps> => {
+  return <div>{<MDXRemote {...source} />}</div>;
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const projectDirectory = path.join(process.cwd(), 'src', 'projects');
+  const filenames = fs.readdirSync(projectDirectory);
+
+  const slugs = filenames.map((name) => {
+    const filePath = path.join(projectDirectory, name);
+    const file = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(file);
+    return data;
+  });
+  return {
+    paths: slugs.map((s) => ({ params: { slug: s.slug } })), // { params: { slug: 'my-first-post' } }
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (!params || params.slug === undefined) throw new Error('Invalid page');
+  const projectDirectory = path.join(
+    process.cwd(),
+    'src',
+    'projects',
+    params.slug + '.mdx'
+  );
+  const project = fs.readFileSync(projectDirectory, 'utf8');
+  const mdxSource = await serialize(project, { parseFrontmatter: true });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: mdxSource.frontmatter,
+    },
+  };
+};
+
+export default ShowcasePage;

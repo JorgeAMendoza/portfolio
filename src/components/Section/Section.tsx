162cd294ad2fbcb9366c08cf86b474c448e283d9
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-06-19 08:56:58 -0500

    install the react-intersection-observer hook, implement the hook to reveal the section on browser view in the Section hook

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import styled from './section.module.css';
+
+interface SectionProps {
+  children: React.ReactNode;
+}
+
+const Section = ({ children }: SectionProps) => {
+  const [showContent, setShowContent] = useState(false);
+  const { ref, inView } = useInView({ threshold: 0.3 });
+  useEffect(() => {
+    if (inView) {
+      setShowContent(true);
+    }
+  }, [inView]);
+  return (
+    <div
+      ref={ref}
+      className={`${styled.section} ${showContent ? styled.showSection : ''}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Section;

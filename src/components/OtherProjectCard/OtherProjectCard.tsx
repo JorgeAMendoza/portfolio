bb94581f5f74be27b15d3ecf7b70065e1f353c12
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-06-21 05:24:36 -0500

    change name of project card

---
+++ src/components/OtherProjectCard/OtherProjectCard.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import styles from './OtherProjectCard.module.css';
+import { Space_Mono } from 'next/font/google';
+
+const SpaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
+interface ProjectCardProps {
+  project: ProjectInfo;
+}
+
+const OtherProjectCard = ({ project }: ProjectCardProps) => {
+  return (
+    <figure className={styles.projectCard}>
+      <div className={styles.projectTitle}>
+        <h3>{project.name}</h3>
+        <div className={styles.projectLinks}>
+          <span>
+            <a
+              href={project.repoLink}
+              target="_blank"
+              aria-label={`link to github repository for ${project.name}`}
+            >
+              <Image
+                src="/icons/github-icon.svg"
+                alt="github icon"
+                width={25}
+                height={25}
+              />
+            </a>
+          </span>
+          <span>
+            <a
+              href={project.demoLink}
+              target="_blank"
+              aria-label={`link to project preview for ${project.name}`}
+            >
+              <Image
+                src="/icons/link-icon.svg"
+                alt="link icon"
+                width={25}
+                height={25}
+              />
+            </a>
+          </span>
+        </div>
+      </div>
+
+      <div>
+        <p>{project.description}</p>
+      </div>
+
+      <footer className={styles.projectFooter}>
+        <ul className={`${styles.projectTools} ${SpaceMono.className}`}>
+          {project.tools.map((tool) => (
+            <li key={tool}>{tool}</li>
+          ))}
+        </ul>
+      </footer>
+    </figure>
+  );
+};
+
+export default OtherProjectCard;
